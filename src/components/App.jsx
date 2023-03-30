d8bbdf221a3263df84099a62c82d9262b37c9901
Artem Salnikov <88932070+wunschpunsh@users.noreply.github.com>
2023-03-30 22:34:10 +0300

    подготавливает проект

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import {GlobalStyles} from './style';
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <div>h1</div>;
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/style.js
@@ -0,0 +1,37 @@
+import {createGlobalStyle} from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+html {
+  height: 100%;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+body,
+html {
+  margin: 0;
+  padding: 0;
+  min-height: 100vh;
+}
+
+body {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+img,
+video {
+  display: block;
+  max-width: 100%;
+  height: auto;
+}
+`;
+
+export {GlobalStyles};
